9c7c0e2de6de0928d2bd318b470550f016454977
Varelfebrianda <varelfebriandaputra@gmail.com>
2026-06-22 13:11:28 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+interface daftar {
+    id:number;
+    nama:string;
+    nilai:number;
+}
+
+function DaftarSiswa(){
+    const siswa :daftar[]=[
+        {id:1, nama:'Varel',nilai:80},
+        {id:2, nama:'Owen',nilai: 90},
+        {id:3, nama:'Alex',nilai: 85},
+        {id:4, nama:'Afdhal',nilai: 70},
+        {id:5, nama:'Christian',nilai: 75}
+    ]
+    const siswaLulus = siswa.filter((x)=>x.nilai >= 80)
+    return(
+        <>
+        <h3>Siswa yang lulus dengan pendapatan nilai diatas 80:</h3>
+        <ul className="daftar">
+            {siswaLulus.map((x)=>(
+            <li key={x.id}>
+                {x.nama} - {x.nilai}
+            </li>
+            ))}
+        </ul>
+        </>
+    )
+}
+
+export default DaftarSiswa
+
